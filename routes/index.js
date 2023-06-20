d54b9c0b03e84b1a1f604affdb3184b329c25514
christina172 <117263200+christina172@users.noreply.github.com>
2023-06-20 15:36:04 +0300

    Display messages on the index page, add a /new route with a form to submit messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const format = require('date-fns/format');
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: format(new Date(), "PPPpp")
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: format(new Date(), "PPPpp")
+  }
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: "Mini Message board", messages: messages });
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('form', { messages: messages });
+});
+
+router.post('/new', function (req, res, next) {
+  messages.push({ text: req.body.message, user: req.body.name, added: format(new Date(), "PPPpp") });
+  res.redirect('/');
 });
 
 module.exports = router;
